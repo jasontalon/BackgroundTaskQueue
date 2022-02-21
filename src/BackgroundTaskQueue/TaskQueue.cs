5087f6d23e3dae2840fd2e55020b8b4c97caf716
Jason Talon <jason.talon@radix.com.au>
2022-02-21 19:53:45 +0800

    add batch dequeue

--- src/BackgroundTaskQueue/TaskQueue.cs
+++ src/BackgroundTaskQueue/TaskQueue.cs
@@ -10,6 +10,9 @@ public interface ITaskQueue
     ValueTask<Func<CancellationToken, ValueTask>> DequeueAsync(
         CancellationToken cancellationToken);
 
+    ValueTask<List<Func<CancellationToken, ValueTask>>> BatchDequeueAsync(int batchSize, TimeSpan timeOut,
+        CancellationToken cancellationToken = default);
+
     ValueTask QueueNotification(INotification notification);
 }
 
@@ -26,6 +29,43 @@ public class TaskQueue : ITaskQueue
         };
         _queue = Channel.CreateBounded<Func<CancellationToken, ValueTask>>(options);
         _mediator = mediator;
+    }
+
+    //https://stackoverflow.com/questions/63881607/how-to-read-remaining-items-in-channel-less-than-batch-size-if-there-is-no-new
+    public async ValueTask<List<Func<CancellationToken, ValueTask>>> BatchDequeueAsync(int batchSize,
+        TimeSpan timeOut,
+        CancellationToken cancellationToken = default)
+    {
+        var workItems = new List<Func<CancellationToken, ValueTask>>();
+
+        using var linkedCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        linkedCancellationTokenSource.CancelAfter(timeOut);
+
+        while (true)
+        {
+            var _cancellationToken = workItems.Count == 0 ? cancellationToken : linkedCancellationTokenSource.Token;
+            Func<CancellationToken, ValueTask> workItem;
+
+            try
+            {
+                workItem = await _queue.Reader.ReadAsync(_cancellationToken).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                break; // The cancellation was induced by timeout (ignore it)
+            }
+            catch (ChannelClosedException)
+            {
+                if (workItems.Count == 0) throw;
+                break;
+            }
+
+            workItems.Add(workItem);
+            if (workItems.Count >= batchSize) break;
+        }
+
+        return workItems;
     }
 
     public async ValueTask QueueNotification(
